2dc86e23fc932e319a83ba9ffc3ae5fe486a9bbf
digpatel123 <digpatel123@gmail.com>
2023-12-21 13:43:05 +0000

    Updated to get the percentages of the stock price difference and get the news of the stock.

--- main.py
+++ main.py
@@ -1,14 +1,62 @@
+import requests
 from dotenv import load_dotenv
-load_dotenv()
+load_dotenv()  # take environment variables from .env.
+import os
 
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
+ALPHA_API_KEY = os.getenv("ALPHA_API_KEY")
+ALPHA_ENDPOINT = "https://www.alphavantage.co/query"
+NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+NEWS_API_KEY = os.getenv("NEWS_API_KEY")
 
 ## STEP 1: Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+stock_params = {
+    "function": "TIME_SERIES_DAILY",
+    "symbol": STOCK,
+    "apikey": ALPHA_API_KEY
+}
+
+response = requests.get(ALPHA_ENDPOINT, params=stock_params)
+data = response.json()["Time Series (Daily)"]
+data_list = [value for (key, value) in data.items()]
+
+# Get yesterday's closing stock price
+yesterday_data = data_list[0]
+yesterday_closing_price = yesterday_data["4. close"]
+
+# Get the day before yesterday's closing stock price
+day_before_yesterday_data = data_list[1]
+day_before_yesterday_closing_price = day_before_yesterday_data["4. close"]
+
+# Find the  difference
+difference = float(yesterday_closing_price) - float(day_before_yesterday_closing_price)
+up_down = None
+if difference > 0:
+    up_down = "🔺"
+else:
+    up_down = "🔻"
+
+# Work out the percentage difference in price between closing price yesterday and closing price the day
+# after yesterday.
+difference_percentage = (difference / float(yesterday_closing_price)) * 100
+print(difference_percentage)
+
+#If difference percentage is greater than 5 then print("Get News").
+if abs(difference_percentage) > 5:
+    print("Get News")
 
 ## STEP 2: Use https://newsapi.org
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
+news_params = {
+    "apiKey": NEWS_API_KEY,
+    "q": COMPANY_NAME
+}
+
+response = requests.get(NEWS_ENDPOINT, params=news_params)
+data = response.json()["articles"][:3]
+
 
 ## STEP 3: Use https://www.twilio.com
 # Send a seperate message with the percentage change and each article's title and description to your phone number. 
@@ -24,4 +72,3 @@ or
 Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
 Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
 """
-
