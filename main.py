4875655c946492acb6c89feab9aee7e3fce2ae4b
digpatel123 <digpatel123@gmail.com>
2024-01-06 22:56:47 +0000

    Completed

--- main.py
+++ main.py
@@ -2,13 +2,20 @@ import requests
 from dotenv import load_dotenv
 load_dotenv()  # take environment variables from .env.
 import os
+from twilio.rest import Client
 
+# Stock Parameters
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
+# Alpha Vantage
 ALPHA_API_KEY = os.getenv("ALPHA_API_KEY")
 ALPHA_ENDPOINT = "https://www.alphavantage.co/query"
+# News API
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 NEWS_API_KEY = os.getenv("NEWS_API_KEY")
+# twilio account
+twilio_account_sid = os.getenv("TWILIO_ACCOUNT_SID")
+twillio_auth_token = os.getenv("TWILIO_AUTH_TOKEN")
 
 ## STEP 1: Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
@@ -61,14 +68,14 @@ data = response.json()["articles"][:3]
 ## STEP 3: Use https://www.twilio.com
 # Send a seperate message with the percentage change and each article's title and description to your phone number. 
 
+# Format of the message
+formatted_articles = [f"{STOCK}: {up_down}{difference_percentage}%\nHeadline: {article['title']}\nBrief: {article['description']}" for article in data]
 
-#Optional: Format the SMS message like this: 
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
+for article in data:
+    client = Client(twilio_account_sid, twillio_auth_token)
+    message = client.messages.create(
+        body= formatted_articles,
+        from_='+1 555 555-5555',
+        to='+1 555 555-5555')
+
+
